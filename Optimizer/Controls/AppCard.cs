8b8a8a03eee90128eb3907303de48292816d7519
Ken <98230827+ItzmeKen@users.noreply.github.com>
2022-09-24 19:44:44 +0530

    Create AppCard.cs

---
+++ Optimizer/Controls/AppCard.cs
@@ -0,0 +1,12 @@
+using System.Windows.Forms;
+
+namespace Optimizer
+{
+    public partial class AppCard : UserControl
+    {
+        public AppCard()
+        {
+            InitializeComponent();
+        }
+    }
+}
